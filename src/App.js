e84d9f21049607fa30df5a0c1d1ec9a7c1a3d7f3
gavrilciunterei <gavriltaller@gmail.com>
2020-10-05 20:52:44 +0300

    NavBar + Pagina seleccionada

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { Router } from "@reach/router";
 
+import { NavBar } from "./components/NavBar";
 import Logo from "./components/Logo";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import { fromPromise } from "@apollo/react-hooks";
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <NavBar />
     </div>
   );
 }
