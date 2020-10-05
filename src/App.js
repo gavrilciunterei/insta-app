3147b208dfa91083753164b87ddd0c676d20fce3
gavrilciunterei <gavriltaller@gmail.com>
2020-10-05 20:09:41 +0300

    Añadido router, link en detail, home y category

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import Logo from "./components/Logo";
-import { PhotoCardDetails } from "./components/PhotoCardDetails";
-import Home from "./pages/Home";
 import { Router } from "@reach/router";
 
-export default function App() {
-  // sacamos la id de la url
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get("detail");
+import Logo from "./components/Logo";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
+export default function App() {
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {detailId ? (
-        <PhotoCardDetails id={detailId} />
-      ) : (
-        <Router>
-          <Home path="/" />
-          <Home path="/pet/:id" />
-        </Router>
-      )}
+      <Router>
+        <Home path="/" />
+        <Home path="/pet/:id" />
+        <Detail path="/detail/:detailId" />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Detail.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { PhotoCardDetails } from "../components/PhotoCardDetails";
+
+export default function Detail({ detailId }) {
+  return <PhotoCardDetails id={detailId} />;
+}
